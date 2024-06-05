c52422f33f61ea15b4e587c0dcbd201f1e5d5c44
Joset2804 <joset2804@hotmail.com>
2024-06-05 18:59:17 -0500

    [FIX]: Fix Form Resource

--- src/main/java/com/agiletech/arteria_api/form/domain/model/entity/Form.java
+++ src/main/java/com/agiletech/arteria_api/form/domain/model/entity/Form.java
@@ -24,16 +24,12 @@ public class Form extends AuditModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
     private Float height;
 
-    @NotNull
     private Float weight;
 
-    @NotNull
     private Float imc;
 
-    @NotNull
     private Integer age;
 
     @NotNull
@@ -41,69 +37,47 @@ public class Form extends AuditModel {
     @Size(max = 100)
     private String gender;
 
-    @NotNull
     private Integer smoking;
 
-    @NotNull
     private Integer alcoholism;
 
-    @NotNull
     private Integer sedentaryLifestyle;
 
-    @NotNull
     private Integer familyHistoryOfEcv;
 
-    @NotNull
     private Integer diabetesMellitus;
 
-    @NotNull
     private Integer obesity;
 
-    @NotNull
-    @NotBlank
     @Size(max = 10)
     private String bloodPressure;
 
-    @NotNull
     private Integer coronaryCalcium;
 
-    @NotNull
     private Integer triglycerides;
 
-    @NotNull
     private Integer cholesterolTotal;
 
-    @NotNull
     private Float cLDL;
 
-    @NotNull
     private Float cHDL;
 
-    @NotNull
     private Float cReactiveProtein;
 
-    @NotNull
     private Integer heartRate;
 
-    @NotNull
     private Float stSegment;
 
-    @NotNull
     private Float qtInterval;
 
-    @NotNull
     private Float electricShaft;
 
-    @NotNull
     private Float rrInterval;
 
-    @NotNull
     private Float qrsComplex;
 
-    @NotNull
     private Float tWave;
 
-    @NotNull
     private Float prSegmentAnomalies;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
--- src/main/java/com/agiletech/arteria_api/form/resource/UpdateFormResource.java
+++ src/main/java/com/agiletech/arteria_api/form/resource/UpdateFormResource.java
@@ -10,16 +10,12 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 public class UpdateFormResource {
-    @NotNull
     private Float height;
 
-    @NotNull
     private Float weight;
 
-    @NotNull
     private Float imc;
 
-    @NotNull
     private Integer age;
 
     @NotNull
@@ -27,68 +23,46 @@ public class UpdateFormResource {
     @Size(max = 100)
     private String gender;
 
-    @NotNull
     private Integer smoking;
 
-    @NotNull
     private Integer alcoholism;
 
-    @NotNull
     private Integer sedentaryLifestyle;
 
-    @NotNull
     private Integer familyHistoryOfEcv;
 
-    @NotNull
     private Integer diabetesMellitus;
 
-    @NotNull
     private Integer obesity;
 
-    @NotNull
-    @NotBlank
     @Size(max = 10)
     private String bloodPressure;
 
-    @NotNull
     private Integer coronaryCalcium;
 
-    @NotNull
     private Integer triglycerides;
 
-    @NotNull
     private Integer cholesterolTotal;
 
-    @NotNull
     private Float cLDL;
 
-    @NotNull
     private Float cHDL;
 
-    @NotNull
     private Float cReactiveProtein;
 
-    @NotNull
     private Integer heartRate;
 
-    @NotNull
     private Float stSegment;
 
-    @NotNull
     private Float qtInterval;
 
-    @NotNull
     private Float electricShaft;
 
-    @NotNull
     private Float rrInterval;
 
-    @NotNull
     private Float qrsComplex;
 
-    @NotNull
     private Float tWave;
 
-    @NotNull
     private Float prSegmentAnomalies;
 }
